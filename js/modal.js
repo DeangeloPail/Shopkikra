34881e13def59fecc0f3a88cb50a8bdb4422c70d
DeangeloPail <deangelo.pail@gmail.com>
2023-03-14 16:46:38 -0400

    primera edicion de git

--- js/modal.js
+++ js/modal.js
@@ -19,7 +19,6 @@ exampleModal.addEventListener('show.bs.modal', event => {
 
   // If necessary, you could initiate an AJAX request here
   // and then do the updating in a callback.
-  //asdsda
   // Update the modal's content.
   const modalTitle = exampleModal.querySelector('.modal-title')
   const usuario = document.getElementById('usuario')
