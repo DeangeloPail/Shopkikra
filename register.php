3aa50d0af8fc0d5f214e8baad474c0cc73646945
DeangeloPail <deangelo.pail@gmail.com>
2023-03-16 22:17:35 -0400

    adaptcion visual de administrador

--- register.php
+++ register.php
@@ -5,7 +5,8 @@
 <html lang="en">
 <head>
 	<title>Shopkikra || Registro</title>
-	<meta charset="UTF-8">
+	<meta charset=
+	"UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 						<!--icono-->	
 	<link rel="icon" type="image/png" href="images/icons/blancoshk.png"/>
